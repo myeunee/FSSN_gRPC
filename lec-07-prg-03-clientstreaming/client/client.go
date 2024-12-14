20accd50edf440ca6c6b3051a8e5a06c8831f01a
myeunee <myeunee@gmail.com>
2024-12-15 03:01:33 +0900

    Update 03-server,client

---
+++ lec-07-prg-03-clientstreaming/client/client.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	pb "github.com/myeunee/FSSN_gRPC/lec-07-prg-03-clientstreaming" // gRPC 코드
+	"google.golang.org/grpc"
+)
+
+func generateMessages() <-chan *pb.Message {
+	messages := []string{
+		"message #1",
+		"message #2",
+		"message #3",
+		"message #4",
+		"message #5",
+	}
+	ch := make(chan *pb.Message)
+	go func() {
+		for _, msg := range messages {
+			fmt.Printf("[client to server] %s\n", msg)
+			ch <- &pb.Message{Message: msg}
+		}
+		close(ch)
+	}()
+	return ch
+}
+
+func sendMessage(client pb.ClientStreamingClient) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	stream, err := client.GetServerResponse(ctx)
+	if err != nil {
+		log.Fatalf("Failed to create stream: %v", err)
+	}
+
+	// 클라이언트에서 메시지 전송
+	for msg := range generateMessages() {
+		if err := stream.Send(msg); err != nil {
+			log.Fatalf("Failed to send message: %v", err)
+		}
+	}
+	response, err := stream.CloseAndRecv() // 서버의 최종 응답 수신
+	if err != nil {
+		log.Fatalf("Failed to receive response: %v", err)
+	}
+	fmt.Printf("[server to client] %d\n", response.GetValue())
+}
+
+func main() {
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Failed to connect to server: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewClientStreamingClient(conn)
+	sendMessage(client)
+}
---
+++ lec-07-prg-03-clientstreaming/server/server.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	pb "github.com/myeunee/FSSN_gRPC/lec-07-prg-03-clientstreaming" // gRPC 코드
+	"google.golang.org/grpc"
+)
+
+type ClientStreamingService struct {
+	pb.UnimplementedClientStreamingServer // 기본 동작 포함
+}
+
+// GetServerResponse handles client-streaming.
+func (s *ClientStreamingService) GetServerResponse(stream pb.ClientStreaming_GetServerResponseServer) error {
+	fmt.Println("Server processing gRPC client-streaming.")
+	count := 0
+	for {
+		_, err := stream.Recv() // 클라이언트로부터 메시지 수신
+		if err != nil {
+			if err.Error() == "EOF" {
+				break // 스트리밍 종료
+			}
+			return err
+		}
+		count++
+	}
+	response := &pb.Number{Value: int32(count)}
+	fmt.Printf("[server to client] %d\n", response.Value)
+	return stream.SendAndClose(response) // 응답 전송 및 스트리밍 닫기
+}
+
+func main() {
+	listener, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("Failed to listen on port 50051: %v", err)
+	}
+
+	server := grpc.NewServer()
+	pb.RegisterClientStreamingServer(server, &ClientStreamingService{})
+	fmt.Println("Starting server. Listening on port 50051.")
+	if err := server.Serve(listener); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
